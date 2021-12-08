ad0cf0c5959b4d7d2c2930217ed435082f0743a7
heenalsapovadia <heenalsapovadia@gmail.com>
2021-12-08 01:52:59 -0400

    Backend changes

--- backend/lambda/index.js
+++ backend/lambda/index.js
@@ -22,7 +22,6 @@ exports.handler = async (event) => {
   let eventObj = event.requestContext.http;
   let requestParams = event.queryStringParameters;
 
-  // if (event.path === "/transactions" && event.httpMethod === "GET") {
   if (eventObj.method === "GET" && eventObj.path === "/transactions") {
     let userId = requestParams.userId;
     console.log("inside GET : its for user : ", userId);
@@ -69,24 +68,129 @@ exports.handler = async (event) => {
         };
       }
     }
+
+    console.log("before scan ....");
     let scanRes = await fetchTransactions();
+    console.log("scnaRes, ", scanRes);
+
+    if (scanRes.statusCode == 200) {
+      // add call for secondary txn records
+      var paramsSec = {
+        TableName: "transactions",
+        IndexName: "splitUserId-index",
+        KeyConditionExpression: "#splitUserId = :userIdValue",
+        ExpressionAttributeNames: {
+          "#splitUserId": "splitUserId",
+        },
+        ExpressionAttributeValues: {
+          ":userIdValue": userId,
+        },
+      };
+      function scan(params) {
+        console.log("inside func scan");
+        return new Promise((resolve, reject) => {
+          console.log("inside new Promise");
+          docClient.query(params, function (err, data) {
+            if (err) {
+              console.log("error : ", JSON.stringify(err, null, 2));
+              console.error(
+                "Unable to query. Error:",
+                JSON.stringify(err, null, 2)
+              );
+              reject({
+                statusCode: 500,
+                body: JSON.stringify({
+                  msg: "fetch Failed",
+                }),
+                headers: { "Content-Type": "application/json" },
+              });
+            } else {
+              console.log("Query succeeded.");
+              // console.log(data.Items);
+              data.Items.forEach(function (item) {
+                console.log(
+                  " -",
+                  item.userID +
+                    ": " +
+                    item.timestamp +
+                    ": " +
+                    item.amount +
+                    ": " +
+                    item.splitUserId
+                );
+              });
+              resolve({
+                statusCode: 200,
+                body: JSON.stringify({
+                  allTxn: data.Items,
+                }),
+                headers: { "Content-Type": "application/json" },
+              });
+            }
+          });
+        });
+      }
+      console.log("before scan ....");
+      let secRes = await scan(paramsSec);
+      console.log("scnaRes, ", secRes);
+
+      console.log("after scan ....");
+      if (secRes.statusCode == 200) {
+        console.log(
+          "inside sec response 200 : ",
+          JSON.parse(secRes.body).allTxn
+        );
+        let newTxnList = JSON.parse(secRes.body).allTxn.map((txn) => {
+          let tempUserId = txn.splitUserId;
+          let tempSplitUserId = txn.userId;
+          return {
+            userId: tempUserId,
+            splitUserId: tempSplitUserId,
+            amount: -1 * txn.amount,
+            description: txn.description,
+            timestamp: txn.timestamp,
+          };
+        });
+
+        return {
+          statusCode: 200,
+          body: JSON.stringify({
+            allTxn: [...JSON.parse(scanRes.body).allTxn, ...newTxnList],
+          }),
+          headers: { "Content-Type": "application/json" },
+        };
+      } else {
+        return secRes;
+      }
+    } else {
+      return scanRes;
+    }
+
     return scanRes;
   }
+
   if (eventObj.method === "POST" && eventObj.path === "/transactions") {
     let userId = requestParams.userId;
+
     console.log("from inside POST!!!", requestParams.userId);
     console.log("body : ", JSON.parse(event.body));
 
     let txnDate = "08-12-2021";
     let reqBody = JSON.parse(event.body);
+    let splitUserId = reqBody.splitUserId;
+    if (reqBody.amount < 0) {
+      userId = reqBody.splitUserId;
+      splitUserId = requestParams.userId;
+    }
+
     var params = {
       TableName: "transactions",
       Item: {
         userId: userId,
         timestamp: Date.now().toString(),
-        splitUserId: reqBody.splitUserId,
+        splitUserId: splitUserId,
         isSettled: false,
-        amount: reqBody.amount,
+        amount: Math.abs(reqBody.amount),
         description: reqBody.description,
       },
     };
